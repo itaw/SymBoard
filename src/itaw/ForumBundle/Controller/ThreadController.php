91f2e6189142ed3dd9f1fb28654a43c88bbba2ab
Florian Weber <florian.weber.dd@me.com>
2014-01-11 18:50:04 +0100

    create thread

--- src/itaw/ForumBundle/Controller/ThreadController.php
+++ src/itaw/ForumBundle/Controller/ThreadController.php
@@ -7,9 +7,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class ThreadController extends Controller
 {
 
-    public function createAction()
+    public function createAction($forum_slug)
     {
-        
+        $forum = $this->getDoctrine()->getRepository('itawDataBundle:Forum')->findOneBySlug($forum_slug);
+
+        if (!$forum) {
+            throw $this->createNotFoundException('The requested Forum was not found!');
+        } else {
+            return $this->get('template_controller')->renderTemplate('thread_create.html.twig', array('forum' => $forum));
+        }
     }
 
     public function updateAction($forum_slug, $thread_slug)
